33de9235d65b1bdf7881088062612ed2f6e330b7
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2024-08-06 13:21:17 +0530

    add products

--- src/Screens/Products.jsx
+++ src/Screens/Products.jsx
@@ -1,34 +1,26 @@
-import React from 'react'
-import jackie from '../imgs/jackie.jpg'
+import React, { useState } from 'react'
+import img1 from '../imgs/jackie.jpg'
+import img2 from '../imgs/julie.jpg'
+import img4 from '../imgs/hermoyini.jpg'
+import img5 from '../imgs/wallter.jpg'
+import img3 from '../imgs/harrypotter.jpg'
 import'../Styles/Product.css'
+import { data } from './db'
 const Products = () => {
+const [Products,setProduct] =useState(data)
   return (
     <div className='product-container'>
+     {data.map((item)=>(
       <div className='product-flex-div'>
-        <img src={jackie} alt="jackie" className='product-img'/>
-        <h3>Jackie Chan</h3>
-        <span>⭐⭐⭐⭐12 Reviews</span>
-        <label>$5</label>
+        <img src={item.productImg} alt="jackie" className='product-img'/>
+        <h3>{item.productName}</h3>
+        <span>{item.productRating}</span>
+        <label>{item.productPrice}</label>
+      
       </div>
-      <div className='product-flex-div'>
-        <img src={jackie} alt="jackie" className='product-img'/>
-        <h3>Jackie Chan</h3>
-        <span>⭐⭐⭐⭐12 Reviews</span>
-        <label>$5</label>
+     ))}
       </div>
-      <div className='product-flex-div'>
-        <img src={jackie} alt="jackie" className='product-img'/>
-        <h3>Jackie Chan</h3>
-        <span>⭐⭐⭐⭐12 Reviews</span>
-        <label>$5</label>
-      </div>
-      <div className='product-flex-div'>
-        <img src={jackie} alt="jackie" className='product-img'/>
-        <h3>Jackie Chan</h3>
-        <span>⭐⭐⭐⭐12 Reviews</span>
-        <label>$5</label>
-      </div>
-    </div>
+    
   )
 }
 
---
+++ src/Screens/db.js
@@ -0,0 +1,33 @@
+import img1 from '../imgs/julie.jpg'
+export const data =[
+    {
+        productImg:"img1",
+        productName:"Ponovo Lavalier Microphone",
+        productRating:"⭐⭐⭐⭐12 Reviews",
+        productPrice:"$10"
+    },
+    {
+        productImg:"img1",
+        productName:"Ponovo Lavalier Microphone",
+        productRating:"⭐⭐⭐⭐12 Reviews",
+        productPrice:"$10"
+    },
+    {
+        productImg:"img1",
+        productName:"Ponovo Lavalier Microphone",
+        productRating:"⭐⭐⭐⭐12 Reviews",
+        productPrice:"$10"
+    },
+    {
+        productImg:"img1",
+        productName:"Ponovo Lavalier Microphone",
+        productRating:"⭐⭐⭐⭐12 Reviews",
+        productPrice:"$10"
+    },
+    {
+        productImg:"img1",
+        productName:"Ponovo Lavalier Microphone",
+        productRating:"⭐⭐⭐⭐12 Reviews",
+        productPrice:"$10"
+    },
+]
